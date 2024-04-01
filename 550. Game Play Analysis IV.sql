aec90fe495f5752126d11db54717b99062504112
Aditya Ranjan <adityaranjan92@gmail.com>
2024-04-01 07:38:06 +0530

    Update 550. Game Play Analysis IV.sql

--- 550. Game Play Analysis IV.sql
+++ 550. Game Play Analysis IV.sql
@@ -42,3 +42,15 @@
 -- +-----------+
 -- Explanation: 
 -- Only the player with id 1 logged back in after the first day he had logged in so the answer is 1/3 = 0.33
+
+-- # Write your MySQL query statement below
+
+select 
+round((select count(distinct a.player_id) from Activity a
+inner join 
+(select player_id, min(event_date) as first_logged 
+    from Activity 
+    group by player_id) b on datediff(a.event_date, b.first_logged)=1
+    and a.player_id = b.player_id)
+    /
+    (select count(distinct player_id) from Activity),2) as fraction;
